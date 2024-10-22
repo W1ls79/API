de92eff784571de48eb5673909fcf40fe0131b48
W1ls79 <w.arenale@gmail.com>
2024-10-22 11:23:46 -0500

    Update ShowProducts.js

--- src/components/ShowProducts.js
+++ src/components/ShowProducts.js
@@ -10,7 +10,7 @@ import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 
 const ShowProducts = () => {
-    const url = 'http://localhost:5002/api/products';
+    const url = 'https://9r1t157f5d.execute-api.us-east-1.amazonaws.com/PruebaAPI';
     const [products, setProducts] = useState([]);
     const [formValues, setFormValues] = useState({ id: '', name: '', description: '', price: '' });
     const [operation, setOperation] = useState(1);
