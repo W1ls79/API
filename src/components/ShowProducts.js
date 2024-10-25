7411c25c135f0b1357d5e2ce4ef08406495f39b2
W1ls79 <w.arenale@gmail.com>
2024-10-25 09:00:26 -0500

    Update ShowProducts.js

--- src/components/ShowProducts.js
+++ src/components/ShowProducts.js
@@ -112,7 +112,7 @@ const ShowProducts = () => {
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        puede decir lo que sea
+                        Volviendo apreder BBVA adelante
                     </Typography>
                     <Button variant="contained" color="secondary" onClick={() => openModal(1)}>
                         Añadir Producto
